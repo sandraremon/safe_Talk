8c29261f0a1b73fd6cf8317699695741465eca83
shimaakamalm2-cmd <shimaakamalm2@gmail.com>
2026-05-10 16:11:03 +0300

    edit for the key derivation

--- crypto/key_derivation.py
+++ crypto/key_derivation.py
@@ -1,22 +1,18 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from key_exchange import (
-    shared_key, shared_key_2
-)
+from key_exchange import shared_key, shared_key_2, key_a, key_b
 
-import os
-from cryptography.hazmat.primitives import hashes
-
-def derive(shared_key):
+#this is to take the secret key and scramble it more (removes the mathematical structure)
+def derive(secret: bytes) -> bytes:
+    salt = min(key_a, key_b) + max(key_a, key_b)
     return HKDF(
         algorithm=hashes.SHA256(),
-        length=32,
-        salt=os.urandom(16),  # or shared transcript hash
+        length=32,# it can stretch to the length i want 
+        salt = salt,  # or shared transcript hash
         info=b'handshake data',
-    ).derive(shared_key)
+    ).derive(secret)
 
-derived_key = derive(shared_key)
-derived_key_2 = derive(shared_key_2)
-
+derived_key = derive(shared_key) #alice derived
+derived_key_2 = derive(shared_key_2) #bob derived
 print("Derived key 1:", derived_key)
 print("Derived key 2:", derived_key_2)
